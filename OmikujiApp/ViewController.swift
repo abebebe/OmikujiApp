3a7c0c6fb9d9c7bd75290f7e7cae8014606b2e93
abebebe <abetk83@gmail.com>
2016-11-12 09:40:30 +0900

    Initial Commit

---
+++ OmikujiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OmikujiApp
+//
+//  Created by 阿部 拓磨 on 2016/11/12.
+//  Copyright © 2016年 阿部 拓磨. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
